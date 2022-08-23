3fd64569fc3b879ce2c4c25f40e84d89c8fbe79b
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-08-22 20:38:37 -0700

    auto

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home.js';
-import Number from './components/Number.js';
 import Hello from './components/Hello.js';
-import Color from './components/Color.js'; 
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/number/:number' element={<Number />} />
-          {/*
-            <Route path='/hello/:word' element={<Hello />} />
-            <Route path='/color/:word/:color' element={<Color />} />
-          */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/:word' element={<Hello />} />
+            <Route path='/:word/:color/:bgColor' element={<Hello />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import { useParams } from "react-router"; //Top of App.js
 //Rest of imports here...
     
 const Hello = (props) => { 
-  const { word } = useParams();
+  const { word, color, bgColor } = useParams();
     
   return (
-    <h1>The number is: { word }</h1>
-  );
-}
+    <>
+    {
+       isNaN(word)? // if the param isnt a number 
+      <h1 style={ color? { color: color, backgroundColor: bgColor }
+        :null
+    }> And the word is : { word } </h1>
+      : // if the param is a number
+      <h1> The number is: { word } </h1>
+    }
+    </>
+    )
+  }
 export default Hello 
